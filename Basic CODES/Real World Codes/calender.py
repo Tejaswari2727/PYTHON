cbd9777851844324c51cdaa02ebb270892b9c628
Batti Tejaswari <125174316+Tejaswari2727@users.noreply.github.com>
2024-08-21 11:54:24 +0530

    Add files via upload

---
+++ Basic CODES/Real World Codes/calender.py
@@ -0,0 +1,61 @@
+''''
+import calendar
+yy= 2012
+mm= 12
+print(calendar.month(yy,mm))
+
+
+
+
+
+---------------OP-------------------------
+
+= RESTART: C:/Users/batti/OneDrive/Desktop/practice python/New folder/more practice/calender.py
+   December 2012
+Mo Tu We Th Fr Sa Su
+                1  2
+ 3  4  5  6  7  8  9
+10 11 12 13 14 15 16
+17 18 19 20 21 22 23
+24 25 26 27 28 29 30
+31
+
+
+
+
+import calendar
+year=int(input("enter a year"))
+month=int(input("enter a month"))
+print(calendar.month(year,month))
+
+
+---------------OP-------------------------
+
+= RESTART: C:/Users/batti/OneDrive/Desktop/practice python/New folder/more practice/calender.py
+enter a year2024
+enter a month7
+     July 2024
+Mo Tu We Th Fr Sa Su
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+29 30 31
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
